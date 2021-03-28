04610618c2b6c6aa7aaf147d44e85b3801256ac4
Pierre Bertet <hello@pierre.world>
2021-03-28 19:10:15 +0100

    Yarn v2 (#4)
    Move to Yarn v2 for library and demo/, which makes it possible to use
    the `portal:` protocol and increases installation speed (even though PnP
    is not enabled for now).
    
    The demo now deduplicates react and react-dom, which are required by the
    library as devDendencies (for Jest) and was requiring to copy the
    library in demo/node_modules, then manually delete
    demo/node_modules/use-nft/node_modules/react. This shouldn’t be needed
    anymore and has been removed from the gh-pages GitHub Action.

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,6 +4,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  resolve: { dedupe: ["react", "react-dom"] },
   build: {
     target: "es2019",
   },
