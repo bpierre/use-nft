dd3a0fefbb93fff48da1086570d2eebcfede1d73
Pierre Bertet <hello@pierre.world>
2021-04-01 01:23:54 +0100

    Fix tests & linting
    - Add eslint & jest to the dependencies.
    - Fix ESLint errors.
    - Add more tests.
    - Move frameImage() into its own utility function.

--- src/fetchers/ethers/types.ts
+++ src/fetchers/ethers/types.ts
@@ -3,7 +3,7 @@ import type { BaseProvider } from "@ethersproject/providers"
 import type { Fetcher } from "../../core"
 
 export type EthersFetcherOptions = {
-  ethers: { Contract: typeof Contract }
+  ethers: { Contract: Contract }
   provider: BaseProvider
 }
 export type EthersFetcher = Fetcher<EthersFetcherOptions>
